7f497eb3b3db94e2ce50ecb8443e86c51b5be2e9
sergioleonzea <slmz25leon@gmail.com>
2020-10-15 22:55:48 -0500

    ejercicios seccion 8

---
+++ ejer2.py
@@ -0,0 +1,5 @@
+#Ejercicio 2
+dolar = float(input("Ingrese el valor actual del dolar: "))
+cDolares = float(input("Ingrese la cantidad de Dolares para su conversión: "))
+cPesos = dolar*cDolares
+print(cDolares, "dólares, equivalen a ", cPesos, "Pesos")
---
+++ ejer4.py
@@ -0,0 +1,5 @@
+#Ejercicio 4
+lustro = float(input("Escriba la cantidad de Lustros: "))
+seg_lustro = 60 * 60 * 24 * 365 * 5
+cant_seg = lustro * seg_lustro
+print(lustro, "lustro(s) equivale a: ", cant_seg, "segundos")
